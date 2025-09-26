b694220ed350681de2fd463ff9f733e6475422eb
femboibussyinspector <shlokt177@gmail.com>
2025-09-26 13:44:34 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cookieparser = require('cookie-parser')
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieparser)
+
+
+module.exports = app
